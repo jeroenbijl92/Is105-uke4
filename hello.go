43cf809f5f39e3ec6a8e16f7f13bc1021c3b9589
osy16 <osmany16@student.uia.no>
2017-01-31 11:27:06 +0100

    osy16

--- hello.go
+++ hello.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, IS105 gruppe")
 	fmt.Println("Test")
-fmt.Println("Rvd er her")
+        fmt.Println("Rvd er her")
+        fmt.Println("OSY16 er her")
 
 }
