b0f1d2c8f5348336c2af64dc65a8ce1cf9cef70d
Magnus Erdvik <Magnus Erdvik>
2017-01-31 18:55:36 +0100

    gjorde en endring

--- hello.go
+++ hello.go
@@ -7,6 +7,7 @@ func main() {
 	fmt.Println("Test")
         fmt.Println("Rvd er her")
         fmt.Println("OSY16 er her")
+	fmt.Println("magnus var her")
 fmt.Println("EvaKriz er her")
 fmt.Println("Erlemange er her")
 
